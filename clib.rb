a4f26a40e17eaa416d5a874e0611dcb9b1e712ef
Nicolas <nixmaldonado@gmail.com>
2024-10-17 12:14:55 -0300

    Fix release file name 0.1.0

--- clib.rb
+++ clib.rb
@@ -1,7 +1,7 @@
 class Clib < Formula
   desc "A simple Go CLI Bitcoin application"
   homepage "https://github.com/nixmaldonado/clib"
-  url "https://github.com/nixmaldonado/clib/releases/download/v0.1.0/clib.tar.gz"
+  url "https://github.com/nixmaldonado/clib/releases/download/v0.1.0/clib-0.1.0.tar.gz"
   sha256 "a6ef3f0dcfe7992907aae92d3909780dc1afbcdb459dfabe39594c2258fed6bb"
   version "0.1.0"
 
