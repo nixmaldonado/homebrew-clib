5f29c1ebcc0fbeb8ecc91f9d051bfb9c4660f369
Nicolas <nixmaldonado@gmail.com>
2024-10-17 15:56:02 -0300

    Added hotfix bump version

--- clib.rb
+++ clib.rb
@@ -1,9 +1,9 @@
 class Clib < Formula
   desc "A simple Go CLI Bitcoin application"
   homepage "https://github.com/nixmaldonado/clib"
-  url "https://github.com/nixmaldonado/clib/releases/download/v0.3.0/clib.tar.gz"
+  url "https://github.com/nixmaldonado/clib/releases/download/v0.3.1/clib.tar.gz"
   sha256 "7923b8ec1a8243cd3381e3ece3a8e54d1b3e0b8f0ef5db6f42a14b6c8e21f418"
-  version "0.3.0"
+  version "0.3.1"
 
   def install
     bin.install "clib"
