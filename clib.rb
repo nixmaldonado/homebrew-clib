840bb6ee38732b72f532519cac372f326691f329
Nicolas <nixmaldonado@gmail.com>
2024-10-17 15:50:25 -0300

    Fixed Invalid sha256

--- clib.rb
+++ clib.rb
@@ -2,7 +2,7 @@ class Clib < Formula
   desc "A simple Go CLI Bitcoin application"
   homepage "https://github.com/nixmaldonado/clib"
   url "https://github.com/nixmaldonado/clib/releases/download/v0.3.0/clib.tar.gz"
-  sha256 "5a9ada00cce8bf61c5c92eccf60b7c585b9d18a88e96afd63ceea3b8c8190e7a"
+  sha256 "7923b8ec1a8243cd3381e3ece3a8e54d1b3e0b8f0ef5db6f42a14b6c8e21f418"
   version "0.3.0"
 
   def install
