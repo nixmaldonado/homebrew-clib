6d5726a90124cb0d8571895d9718756c09e4f73c
Nicolas <nixmaldonado@gmail.com>
2024-10-17 11:35:38 -0300

    Added sha-256 for release file v0.1.0

--- clib.rb
+++ clib.rb
@@ -1,11 +1,11 @@
 class Clib < Formula
   desc "A simple Go CLI Bitcoin application"
   homepage "https://github.com/nixmaldonado/clib"
-  url "https://github.com/nixmaldonado/clib/releases/download/v1.0.0/clid.tar.gz"
-  sha256 "replace-with-your-sha256"
-  version "1.0.0"
+  url "https://github.com/nixmaldonado/clib/releases/download/v0.1.0/clib.tar.gz"
+  sha256 "a6ef3f0dcfe7992907aae92d3909780dc1afbcdb459dfabe39594c2258fed6bb"
+  version "0.1.0"
 
   def install
-    bin.install "clid"
+    bin.install "clib"
   end
 end
